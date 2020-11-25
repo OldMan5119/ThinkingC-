916a5729ff0806d2026ce89f20a91b55da3ed5e2
石鑫 <shixin@shixindeMBP.lan>
2020-11-25 23:57:57 +0800

    提交示例代码

---
+++ build/CMakeFiles/ThinkingCplus.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ThinkingCplus.dir/main.cpp.o"
+  "ThinkingCplus"
+  "ThinkingCplus.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ThinkingCplus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
